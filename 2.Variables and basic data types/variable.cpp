a304a765c76ab7172f80437790f960d19a09ac8c
nickthelegend <testingtesla7@gmail.com>
2023-08-04 15:13:46 +0400

    Learnt 2nd Thing

---
+++ 2.Variables and basic data types/variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream> ///this is input output steam library
+
+using namespace std;
+int main(){
+
+ 
+
+  int x = 2;
+  int y = 3;
+  cout<< "X is equal to:" << x << endl;
+  cout<< "Y is equal to:" << y << endl;
+  cout <<"The sum is equal to:"<< x+y<<endl;
+}
